397cdd31abfad1509e35e03541d88f081751c078
Ian Bachiller <ianbachise@gmail.com>
2024-01-31 01:41:42 -0500

    updated typo errors, fixed script.js, now changes seen on website

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElemenet("h2");
-h2.textContent = "This conecnt is added thorugh JavaScript";
-document.querySelectorr("body").appendChild(h2);
+const h2 = document.createElement("h2");
+h2.textContent = "This content is added thorugh JavaScript";
+document.querySelector("body").appendChild(h2);
